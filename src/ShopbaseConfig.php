bd488f1091b2e40d173dddf2ec3be17379275257
AnuragMakol <anurag.makol@outlook.com>
2017-05-22 22:20:56 +0530

    Fixed Config File

--- src/ShopbaseConfig.php
+++ src/ShopbaseConfig.php
@@ -7,7 +7,7 @@ return [
 
     'secret' => '',
 
-    'permission' => 'read_content,write_content,read_themes,write_themes,read_users,write_users,read_orders,write_orders,read_products,write_products,read_scrip_tags,write_script_tags',
+    'permission' => 'read_content,write_content,read_themes,write_themes,read_customers,write_customers,read_orders,write_orders,read_products,write_products,read_script_tags,write_script_tags',
 
     'redirect' => ''
 
